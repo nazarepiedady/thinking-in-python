8d9670ac18f0d825143526480905b41d390557b3
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-12 08:39:06 +0100

    chore: add example for `strings operations`

---
+++ 02-variables-expressions-instructions/operations-strings.py
@@ -0,0 +1,19 @@
+'''
+it cannot possible use mathematical operations
+with strings except `addition` and `multiplication`
+'''
+
+# `addition` example
+# use it to concatenate strings
+throat = 'throat'
+warbler = 'warbler'
+throatwarbler = throat + warbler
+
+print(throatwarbler)
+
+# `multiplication` example
+# use it to repeat the string
+spam = 'spam'
+spam_spam_spam = spam * 3
+
+print(spam_spam_spam)
