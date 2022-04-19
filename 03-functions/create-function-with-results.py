6ae011f9fc75b88a143fed663d26d9176cff6e2b
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-19 09:30:56 +0100

    chore: create an example of a function that return values

---
+++ 03-functions/create-function-with-results.py
@@ -0,0 +1,13 @@
+def addition(first_number, second_number):
+    '''Return the sum between two numbers
+
+    >>> addition(2, 2)
+    4
+    '''
+    return first_number + second_number
+
+
+result = addition(2, 2)
+
+
+print(f'result: {result}')
