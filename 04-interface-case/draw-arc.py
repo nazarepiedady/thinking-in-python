9b5b582669a81d905ccca763f8eaa517bd29e4aa
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-26 15:14:27 +0100

    chore: define the `arc` function
    this function reuse the polygon algorithm to draw a `arc` based on
    the given radius and angle passed with the turtle object.

---
+++ 04-interface-case/draw-arc.py
@@ -0,0 +1,9 @@
+def arc(t, r, angle):
+    from math import pi
+    arc_length = 2 * pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = angle / n
+    for iterator in range(n):
+        t.fd(step_length)
+        t.lt(step_angle)
