83871d56a62928bba4e95eb7229e15bc6b3618e5
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-27 13:10:10 +0100

    chore: add a docstring in the `polyline` method

--- 04-interface-case/polyline.py
+++ 04-interface-case/polyline.py
@@ -1,4 +1,7 @@
 def polyline(t, n, length, angle):
+    '''Draw n rect segments with the given width and
+    angle (in degrees) between them. t is a turtle.
+    '''
     for iterator in range(n):
         t.fd(length)
         t.lt(angle)
