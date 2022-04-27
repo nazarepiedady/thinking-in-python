7947ec040f16eb8c980a41dc32622eb89c2efdca
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-27 11:53:58 +0100

    refactor: abstract a copy of polygon function into polyline
    make the polygon receive a third argument `angle` and use it to
    draw to left.

--- 04-interface-case/draw-arc.py
+++ 04-interface-case/draw-arc.py
@@ -1,5 +1,11 @@
 import turtle
 
+
+def polyline(t, n, length, angle):
+    for iterator in range(n):
+        t.fd(length)
+        t.lt(angle)
+
 def arc(t, r, angle):
     from math import pi
     arc_length = 2 * pi * r * angle / 360
