8738c4c1d05356d95edcfe34c9d154ee556e0e4e
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-27 12:11:46 +0100

    refactor: import `polyline` method from the polyline module
    use it to draw the lines of the `arc` function based on the given
    `step_angle` value.

--- 04-interface-case/draw-arc.py
+++ 04-interface-case/draw-arc.py
@@ -1,24 +1,20 @@
 import turtle
 
 
-def polyline(t, n, length, angle):
-    for iterator in range(n):
-        t.fd(length)
-        t.lt(angle)
-
 def polygon(t, n, length):
     angle = 360 / n
     polyline(t, n, length, angle)
 
 def arc(t, r, angle):
     from math import pi
+    from polyline import polyline
+
     arc_length = 2 * pi * r * angle / 360
     n = int(arc_length / 3) + 1
     step_length = arc_length / n
     step_angle = angle / n
-    for iterator in range(n):
-        t.fd(step_length)
-        t.lt(step_angle)
+
+    polyline(t, n, step_length, step_angle)
 
 
 custom_arc = turtle.Turtle()
