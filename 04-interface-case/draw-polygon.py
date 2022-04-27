4a25618bd2c1afc467ed194317bd6280abf5f453
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-27 12:18:15 +0100

    refactor: import `polyline` function into `draw-polygon` script
    use it to draw a polygon based on the given `polygon_angle`

--- 04-interface-case/draw-polygon.py
+++ 04-interface-case/draw-polygon.py
@@ -1,10 +1,10 @@
 import turtle
 
 def polygon(t, n, length):
+    from polyline import polyline
+
     polygon_angle = 360 / n
-    for iterator in range(n):
-        t.fd(length)
-        t.lt(polygon_angle)
+    polyline(t, n, length, polygon_angle)
 
 bob = turtle.Turtle()
 
