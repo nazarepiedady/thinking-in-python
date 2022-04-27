bf3d49a5a38082fb2681356e57a84f3460cceaf9
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-27 13:14:34 +0100

    refactor: remove the `polygon` function from `draw_circle` script

--- 04-interface-case/draw_circle.py
+++ 04-interface-case/draw_circle.py
@@ -1,10 +1,5 @@
 import turtle
 
-def polygon(t, n, length):
-    polygon_angle = 360 / n
-    for iterator in range(n):
-        t.fd(length)
-        t.lt(polygon_angle)
 
 def circle(t, r):
     from draw_arc import arc
