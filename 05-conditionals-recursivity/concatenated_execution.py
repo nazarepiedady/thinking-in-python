54fa7aac6f71863ba8ead81de38c8df530df3759
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-03 17:18:01 +0100

    chore: write example code of concatenated conditionals usage

---
+++ 05-conditionals-recursivity/concatenated_execution.py
@@ -0,0 +1,9 @@
+first_number = 10
+second_number = 12
+
+if first_number < second_number:
+    print('first_number is less than second_number')
+elif first_number > second_number:
+    print('first_number is greater than second_number')
+else:
+    print('first_number and second_number are equal')
