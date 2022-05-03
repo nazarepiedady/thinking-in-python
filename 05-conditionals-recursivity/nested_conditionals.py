5beb0b41099d4f4ae75aee06a18e24fa01a8c4ef
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-03 17:33:08 +0100

    chore: write example code of nested conditionals usage

---
+++ 05-conditionals-recursivity/nested_conditionals.py
@@ -0,0 +1,10 @@
+first_number = 10
+second_number = 20
+
+if first_number == second_number:
+    print('first_number and second_number are equal')
+else:
+    if first_number < second_number:
+        print('first_number is less than second_number')
+    else:
+        print('first_number is greater than second_number')
