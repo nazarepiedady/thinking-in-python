97744fc47ea2f7a9cef4ff9f033b4629e3700fc1
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-24 17:58:16 +0100

    refactor(compare_two_values.py): remove the usage function example

--- 06-functions-with-result/compare_two_values.py
+++ 06-functions-with-result/compare_two_values.py
@@ -5,12 +5,3 @@ def compare_two_values(first_value, second_value):
         return 0
     else:
         return -1
-
-
-two_bigger_than_one = compare_two_values(2, 1)
-two_is_equal_two = compare_two_values(2, 2)
-two_smaller_than_three = compare_two_values(2, 3)
-
-for result in [two_bigger_than_one, two_is_equal_two,
-                two_smaller_than_three]:
-    print(result, end=' ')
