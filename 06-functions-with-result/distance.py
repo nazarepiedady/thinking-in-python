3687d1b0aca11110975a8c467c2e5efb0603408b
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-25 15:01:44 +0100

    chore: print the value of the two distances

--- 06-functions-with-result/distance.py
+++ 06-functions-with-result/distance.py
@@ -1,3 +1,6 @@
 def calculate_distance(x1, y1, x2, y2):
     horizontal_distance = x2 - x1
     vertical_distance = y2 - y1
+    print('horizontal_distance is', horizontal_distance)
+    print('vertical_distance is', vertical_distance)
+    return 0.0
