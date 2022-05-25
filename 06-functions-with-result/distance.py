14ed8fb8b631cb66148f0dd61b2a6243eb03b54c
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-25 15:08:37 +0100

    chore: calculate the square of horizontal and vertical distance

--- 06-functions-with-result/distance.py
+++ 06-functions-with-result/distance.py
@@ -1,6 +1,5 @@
 def calculate_distance(x1, y1, x2, y2):
     horizontal_distance = x2 - x1
     vertical_distance = y2 - y1
-    print('horizontal_distance is', horizontal_distance)
-    print('vertical_distance is', vertical_distance)
+    squared_distance = horizontal_distance ** 2 + vertical_distance ** 2
     return 0.0
