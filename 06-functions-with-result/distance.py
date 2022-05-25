65d2cc34210eec3c504c8aab3dc6d4fe5b29503f
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-25 15:17:56 +0100

    chore: calculate the square root of the squared distance

--- 06-functions-with-result/distance.py
+++ 06-functions-with-result/distance.py
@@ -4,5 +4,5 @@ def calculate_distance(x1, y1, x2, y2):
     horizontal_distance = x2 - x1
     vertical_distance = y2 - y1
     squared_distance = horizontal_distance ** 2 + vertical_distance ** 2
-    print('squared_distance is', squared_distance)
+    distance = math.sqrt(squared_distance)
     return 0.0
