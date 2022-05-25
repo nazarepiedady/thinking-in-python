5a81f44764e529df8714787a4928eb8fce4c3033
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-25 15:57:55 +0100

    chore: return the circle area

--- 06-functions-with-result/circle_area.py
+++ 06-functions-with-result/circle_area.py
@@ -5,3 +5,4 @@ import distance
 def calculate_circle_area(xc, yc, xp, yp):
     radius = distance.calculate_distance(xc, yc, xp, yp)
     circle_area = area.calculate_area(radius)
+    return circle_area
