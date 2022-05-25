d69b4445e0cb7450eb7bea3b011121081df5c3dd
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-25 16:00:57 +0100

    refactor: remake the circle area example to turn it more concise
    from each module import only the methods that are being used, and
    use them to compose the returned expression of calculate circle
    area function.

--- 06-functions-with-result/circle_area.py
+++ 06-functions-with-result/circle_area.py
@@ -1,8 +1,15 @@
-import area
-import distance
+# import area
+# import distance
+
+
+# def calculate_circle_area(xc, yc, xp, yp):
+#     radius = distance.calculate_distance(xc, yc, xp, yp)
+#     circle_area = area.calculate_area(radius)
+#     return circle_area
+
+from area import calculate_area
+from distance import calculate_distance
 
 
 def calculate_circle_area(xc, yc, xp, yp):
-    radius = distance.calculate_distance(xc, yc, xp, yp)
-    circle_area = area.calculate_area(radius)
-    return circle_area
+    return calculate_area(calculate_distance(xc, yc, xp, yp))
