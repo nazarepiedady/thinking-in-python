15480a9e9672ded7651adb70ac958406233abfdb
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-05-25 15:44:14 +0100

    chore: move the math import out of the function `area`

--- 06-functions-with-result/area.py
+++ 06-functions-with-result/area.py
@@ -1,7 +1,9 @@
+import math
+
+
 def area(radius):
     '''
     receive the `radius` value to return the area
     based on given radius value.
     '''
-    import math
-    return math.pi * radius ** 2
+    return math.pi * (radius ** 2)
