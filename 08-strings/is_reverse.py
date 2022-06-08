8829fe86963b7f7fc2c41a6273d29293e515cdbe
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-08 16:26:07 +0100

    chore: improve the precison of the `is_reverse` function

--- 08-strings/is_reverse.py
+++ 08-strings/is_reverse.py
@@ -3,7 +3,7 @@ def is_reverse(first_word, second_word):
         return False
     first_word_index = 0
     second_word_index = len(second_word) - 1
-    while second_word_index > 0:
+    while second_word_index >= 0:
         if first_word[first_word_index] != second_word[second_word_index]:
             return False
         first_word_index = first_word_index + 1
