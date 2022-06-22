783207b709d4806a4b1f01977002314535ce7b47
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-22 18:20:46 +0100

    feat: implement the function `is_abecedarian_recursive`

--- 09-word-games/is_abecedarian.py
+++ 09-word-games/is_abecedarian.py
@@ -1,7 +1,15 @@
-def is_abecedarian(word):
+def is_abecedarian_loop(word):
     previous = word[0]
     for char in word:
         if char < previous:
             return False
         previous = char
     return True
+
+
+def is_abecedarian_recursive(word):
+    if len(word) <= 1:
+        return True
+    if word[0] > word[1]:
+        return False
+    return is_abecedarian_recursive(word[1:])
