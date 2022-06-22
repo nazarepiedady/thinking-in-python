e13683edd3ee861e85d944fbf0504b2f2a043ce9
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-22 18:29:32 +0100

    feat: implement the function `is_palindrome`

---
+++ 09-word-games/is_palindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(word):
+    first_index = 0
+    second_index = len(word)-1
+    while first_index < second_index:
+        if word(first_index) != word[second_index]
+            return False
+        first_index = first_index + 1
+        second_index = second_index - 1
+    return True
