9c1003733a5e198bbc830645660bead63ba7167b
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-30 19:20:43 +0100

    feat: use the asterisk operator to repeat the elements from a list

--- 10-lists/list_operations.py
+++ 10-lists/list_operations.py
@@ -1,6 +1,10 @@
 numbers = [1, 2, 3]
 more_numbers = [4, 5, 6]
 
+# concatenate lists
 combined_numbers = numbers + more_numbers
 
-print(combined_numbers)
+# repeat the elements inside lists
+repeated_numbers = numbers * 3
+
+print(combined_numbers, repeated_numbers)
