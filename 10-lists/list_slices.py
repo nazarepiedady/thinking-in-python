288cd3997f76b8f304ca32b6f69ae55cc28689bd
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-30 19:33:41 +0100

    feat: implement a slice of the list with only the last edge

--- 10-lists/list_slices.py
+++ 10-lists/list_slices.py
@@ -2,9 +2,11 @@ alphabet_letters = ['a', 'b', 'c', 'd', 'e', 'f']
 
 # start on the second to third
 second_third_letters = alphabet_letters[1:3]
+# slice the first four elements
+first_four_letters = alphabet_letters[:4]
 
 
 print(
-    second_third_letters, #first_four_letters,
+    second_third_letters, first_four_letters,
     #from_third_letters, alphabet_letters_copy
 )
