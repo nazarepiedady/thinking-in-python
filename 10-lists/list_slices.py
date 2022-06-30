7cb6f448ab509246845b07555a2222b13bcba755
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-30 19:33:02 +0100

    feat: implement a slice of the list with two edges

---
+++ 10-lists/list_slices.py
@@ -0,0 +1,10 @@
+alphabet_letters = ['a', 'b', 'c', 'd', 'e', 'f']
+
+# start on the second to third
+second_third_letters = alphabet_letters[1:3]
+
+
+print(
+    second_third_letters, #first_four_letters,
+    #from_third_letters, alphabet_letters_copy
+)
