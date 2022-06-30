f793027b967350d4976d7654bc106211f1ba4488
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-30 19:34:40 +0100

    feat: implement a slice of the list with no edge

--- 10-lists/list_slices.py
+++ 10-lists/list_slices.py
@@ -6,10 +6,11 @@ second_third_letters = alphabet_letters[1:3]
 first_four_letters = alphabet_letters[:4]
 # slice from third index element to the end
 from_third_letters = alphabet_letters[3:]
-
+# make a copy of the previous list
+alphabet_letters_copy = alphabet_letters[:]
 
 
 print(
     second_third_letters, first_four_letters,
-    from_third_letters, #alphabet_letters_copy
+    from_third_letters, alphabet_letters_copy
 )
