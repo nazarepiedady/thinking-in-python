60e2a19e26a5196f970558fcb870c34bb1a0b15b
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-19 19:10:48 +0100

    feat: use `extend` list method to extend the `alphabet_letters` list

--- 10-lists/list_methods.py
+++ 10-lists/list_methods.py
@@ -1,4 +1,8 @@
 alphabet_letters = ['a', 'b', 'c']
 alphabet_letters.append('d')
+print(alphabet_letters)
 
+alphabet_letters = ['a', 'b', 'c']
+more_alphabet_letters = ['d', 'e']
+alphabet_letters.extend(more_alphabet_letters)
 print(alphabet_letters)
