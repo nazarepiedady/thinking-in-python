10cd20fd5258b901d60218957bdabd60ad2e3780
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-19 19:22:17 +0100

    chore: separate the variables printing

--- 10-lists/list_operations.py
+++ 10-lists/list_operations.py
@@ -3,8 +3,8 @@ more_numbers = [4, 5, 6]
 
 # concatenate lists
 combined_numbers = numbers + more_numbers
+print(combined_numbers)
 
 # repeat the elements inside lists
 repeated_numbers = numbers * 3
-
-print(combined_numbers, repeated_numbers)
+print(repeated_numbers)
