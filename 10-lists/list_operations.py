c8b53e55cfacbdc6f8bbb249e3948750af7f5e58
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-19 19:24:28 +0100

    feat: use `sum` built-in function to sum all number in the list `numbers`

--- 10-lists/list_operations.py
+++ 10-lists/list_operations.py
@@ -8,3 +8,8 @@ print(combined_numbers)
 # repeat the elements inside lists
 repeated_numbers = numbers * 3
 print(repeated_numbers)
+
+
+# sum all numeric elements in the list
+result_sumed_number = sum(numbers)
+print(result_sumed_number)
