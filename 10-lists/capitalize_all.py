a1095cd01d033307a9766a93a7010b5bb3c97219
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-19 19:30:52 +0100

    feat: implement the `capitalize_all` function to capitalize all strings into a list

---
+++ 10-lists/capitalize_all.py
@@ -0,0 +1,5 @@
+def capitalize_all(strings_collection):
+    modified_strings_collection = []
+    for string in strings_collection:
+        modified_strings_collection.append(string.capitalize())
+    return modified_strings_collection
