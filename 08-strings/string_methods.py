f03368fcc509c4949449bf11cf1b7cbdfed87c54
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-26 13:36:19 +0100

    chore: adjust the order of variables declarations in `string_methods.py` script

--- 08-strings/string_methods.py
+++ 08-strings/string_methods.py
@@ -2,7 +2,7 @@ sentence = 'pining for the fjords'
 words_list = sentence.split()
 print(words_list)
 
-alphabet_letters = ['a', 'b', 'c', 'd', 'e', 'f']
 delimiter = ' '
+alphabet_letters = ['a', 'b', 'c', 'd', 'e', 'f']
 alphabet_letters_string = delimiter.join(alphabet_letters)
 print(alphabet_letters_string)
