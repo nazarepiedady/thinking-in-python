ffe80a5eb8bc2229cf84b08f10e8e19a3a922914
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-07-26 13:35:07 +0100

    chore: use `str.join(_list)` to transform `alphabet_letters` list into a string

--- 08-strings/string_methods.py
+++ 08-strings/string_methods.py
@@ -1,3 +1,8 @@
 sentence = 'pining for the fjords'
 words_list = sentence.split()
 print(words_list)
+
+alphabet_letters = ['a', 'b', 'c', 'd', 'e', 'f']
+delimiter = ' '
+alphabet_letters_string = delimiter.join(alphabet_letters)
+print(alphabet_letters_string)
